fdb82a0b079ea48fffbd381c556c981a9e157fd8
wander4747 <wander.douglas14@gmail.com>
2021-04-21 14:11:01 -0300

    create order handler kafka

--- customer_service/app/Kafka/OrderHandler.php
+++ customer_service/app/Kafka/OrderHandler.php
@@ -1,7 +1,19 @@
 <?php
 
+namespace App\Kafka;
 
-class OrderHandler
+use PHPEasykafka\KafkaConsumerHandlerInterface;
+
+class OrderHandler implements KafkaConsumerHandlerInterface
 {
 
+    public function __invoke(\RdKafka\Message $message, \RdKafka\KafkaConsumer $consumer)
+    {
+        $payload = json_decode($message->payload);
+        print_r(json_decode($message->payload));
+
+        //$orderService = new OrderService($payload);
+        //$orderService->insert();
+//        $consumer->commit();
+    }
 }
